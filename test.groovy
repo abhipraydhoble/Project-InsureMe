054ff3c3bf5e9bb435e4acc2e055b9cad2c7da21
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-08-26 11:23:35 +0530

    Update and rename code-b35.md to test.groovy

---
+++ test.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    tools{
+        maven 'maven'
+    }
+    
+    stages{
+        stage('git'){
+            steps{
+                git branch: 'main', changelog: false, poll: false, url: 'https://github.com/abhipraydhoble/Project-InsureMe.git'
+            }
+        }
+        stage('build'){
+            steps{
+                sh 'mvn clean package'
+            }
+        }
+        stage ('image'){
+            steps{
+                sh 'docker build -t testapp .'
+            }
+        }
+        stage('deploy'){
+            steps{
+                sh ' docker run -itd -p 8089:8081 testapp'
+            }
+        }
+    }
+}
